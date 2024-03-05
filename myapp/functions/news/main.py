40b68df0744a0fc45391973c7a0bf5063238db84
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-03-05 15:39:18 -0700

    news route

--- myapp/functions/news/main.py
+++ myapp/functions/news/main.py
@@ -34,7 +34,30 @@ def news_manager(request):
 
     uid = decoded_token['uid']
     news_service = NewsService(db, uid)
+
     if request.path == '/':
         news_data = news_service.get_all_news_articles(uid)
-        return news_data, 200
+        return (news_data, 200, headers)
 
+    if request.path == '/get-articles':
+        data = request.get_json()
+        query = data['query']
+        urls = news_service.get_article_urls(query)
+        news_data = news_service.summarize_articles(urls)
+        news_service.upload_news_data(uid, news_data)
+
+        return (news_data, 200, headers)
+    
+    if request.path == '/get-news-topics':
+        news_topics = news_service.get_user_news_topics(uid)
+        return (news_topics, 200, headers)
+    
+    if request.path == '/news_articles':
+        data = request.get_json()
+        doc_id = data['articleId']
+        if request.method == 'PUT':
+            news_service.mark_is_read(uid, doc_id)
+            return ("Updated successfully", 200, headers)
+        if request.method == 'DELETE':
+            news_service.delete_news_article(uid, doc_id)
+            return ("Deleted successfully", 200, headers)
