dd15b25dd8895eaafb2df6f757c8c0c8ac2236f2
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-07-27 20:07:56 -0600

    display page # and source of current previewed url

--- src/dashboards/knowledgeBase/components/KbDocCard.js
+++ src/dashboards/knowledgeBase/components/KbDocCard.js
@@ -1,31 +1,54 @@
-import { useContext, useState, useMemo } from 'react';
+import { useContext, useState, useMemo, useEffect } from 'react';
 import {
     Box,
-    Typography,
     Card,
     CardContent,
     CardHeader,
     CardActions,
+    IconButton,
+    Typography,
+    Tooltip,
 } from '@mui/material';
-import { Delete, OpenInNew } from '@mui/icons-material/';
+import {
+    Delete,
+    OpenInNew,
+    ArrowBackIos,
+    ArrowForwardIos,
+} from '@mui/icons-material';
+
 import { KbContext } from '../../../contexts/KbContext';
 import { StyledIconButton } from '../../chat/chatStyledComponents';
 import TextEditor from './textEditor/TextEditor';
 import Markdown from 'react-markdown';
 
 const KbDocCard = ({ document }) => {
+    const [currentUrlIndex, setCurrentUrlIndex] = useState(0);
     const [isEditorOpen, setIsEditorOpen] = useState(false);
     const { deleteKbDoc } = useContext(KbContext);
 
-    const matchingContent = useMemo(() => {
-        const stripProtocol = (url) =>
-            url.replace(/^(https?:\/\/)?(www\.)?/, '');
-        const docSource = stripProtocol(document.source);
-        const matchingUrl = document.urls.find(
-            (url) => stripProtocol(url.source) === docSource
+    const urls = useMemo(() => document.urls || [], [document]);
+
+    useEffect(() => {
+        setCurrentUrlIndex(urls.length - 1);
+    }, [urls]);
+
+    const currentContent = useMemo(() => {
+        if (urls.length === 0) return '';
+        return urls[currentUrlIndex].content || '';
+    }, [urls, currentUrlIndex]);
+
+    const handlePrevUrl = () => {
+        setCurrentUrlIndex((prevIndex) =>
+            prevIndex < urls.length - 1 ? prevIndex + 1 : 0
         );
-        return matchingUrl ? matchingUrl.content : '';
-    }, [document]);
+    };
+
+    const handleNextUrl = () => {
+        setCurrentUrlIndex((prevIndex) =>
+            prevIndex > 0 ? prevIndex - 1 : urls.length - 1
+        );
+    };
+
     const handleDelete = () => {
         deleteKbDoc(document.kb_id, document.id);
     };
@@ -43,7 +66,38 @@ const KbDocCard = ({ document }) => {
             }}
             elevation={6}
         >
-            <CardHeader title="Document Source" subheader={document.source} />
+            <CardHeader
+                title="Document Source"
+                subheader={
+                    <>
+                        <Tooltip title={urls[currentUrlIndex].source}>
+                            <Typography
+                                variant="body2"
+                                sx={{
+                                    maxWidth: '90%',
+                                    display: 'inline-block',
+                                    verticalAlign: 'middle',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    whiteSpace: 'nowrap',
+                                }}
+                            >
+                                {urls[currentUrlIndex].source}
+                            </Typography>
+                        </Tooltip>
+                        {urls.length > 1 && (
+                            <Typography
+                                variant="caption"
+                                display="block"
+                                sx={{ mt: 0.5 }}
+                            >
+                                Page {urls.length - currentUrlIndex} of{' '}
+                                {urls.length}
+                            </Typography>
+                        )}
+                    </>
+                }
+            />
             <CardContent>
                 <Box
                     sx={{
@@ -56,18 +110,74 @@ const KbDocCard = ({ document }) => {
                         backgroundColor: 'background.paper',
                         borderRadius: 2,
                         boxShadow: 1,
+                        overflowX: 'hidden',
                     }}
                 >
-                    <Markdown>{matchingContent}</Markdown>
+                    <Markdown
+                        components={{
+                            pre: ({ node, ...props }) => (
+                                <pre
+                                    style={{
+                                        overflowX: 'auto',
+                                        maxWidth: '100%',
+                                    }}
+                                    {...props}
+                                />
+                            ),
+                            code: ({ node, ...props }) => (
+                                <code
+                                    style={{
+                                        wordBreak: 'break-word',
+                                        whiteSpace: 'pre-wrap',
+                                    }}
+                                    {...props}
+                                />
+                            ),
+                            img: ({ node, ...props }) => (
+                                <img
+                                    alt={props.alt}
+                                    style={{
+                                        maxWidth: '100%',
+                                        height: 'auto',
+                                        objectFit: 'contain',
+                                    }}
+                                    {...props}
+                                />
+                            ),
+                            p: ({ node, ...props }) => (
+                                <p
+                                    style={{
+                                        overflowWrap: 'break-word',
+                                        wordWrap: 'break-word',
+                                    }}
+                                    {...props}
+                                />
+                            ),
+                        }}
+                    >
+                        {currentContent}
+                    </Markdown>
                 </Box>
             </CardContent>
-            <CardActions>
-                <StyledIconButton onClick={handleDelete}>
-                    <Delete />
-                </StyledIconButton>
-                <StyledIconButton onClick={toggleEditor}>
-                    <OpenInNew />
-                </StyledIconButton>
+            <CardActions sx={{ justifyContent: 'space-between' }}>
+                <Box>
+                    <StyledIconButton onClick={handleDelete}>
+                        <Delete />
+                    </StyledIconButton>
+                    <StyledIconButton onClick={toggleEditor}>
+                        <OpenInNew />
+                    </StyledIconButton>
+                </Box>
+                {urls.length > 1 && (
+                    <Box>
+                        <IconButton onClick={handlePrevUrl}>
+                            <ArrowBackIos />
+                        </IconButton>
+                        <IconButton onClick={handleNextUrl}>
+                            <ArrowForwardIos />
+                        </IconButton>
+                    </Box>
+                )}
             </CardActions>
             {isEditorOpen && (
                 <TextEditor
