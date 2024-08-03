b9ec8f4774194c683e8b787db150743bcd06545d
Shaun Offenbacher <130625311+makeiteasierapps@users.noreply.github.com>
2024-08-02 23:28:28 -0600

    refactored setter functions

--- src/hooks/useImageManager.js
+++ src/hooks/useImageManager.js
@@ -1,174 +1,171 @@
-import { useEffect, useCallback, useState, useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-import { SnackbarContext } from '../contexts/SnackbarContext';
+import { useEffect, useCallback, useState, useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { useSnackbar } from "../contexts/SnackbarContext";
 
 export const useImageManager = () => {
-    const [userPrompt, setUserPrompt] = useState('');
-    const [imageRequest, setImageRequest] = useState(null);
-    const [imageUrl, setImageUrl] = useState(null);
-    const [imageList, setImageList] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const { showSnackbar } = useContext(SnackbarContext);
-    const { uid } = useContext(AuthContext);
+  const [userPrompt, setUserPrompt] = useState("");
+  const [imageRequest, setImageRequest] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageList, setImageList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { uid } = useContext(AuthContext);
+  const { showSnackbar } = useSnackbar();
 
-    const backendUrl =
-        process.env.NODE_ENV === 'development'
-            ? `http://${process.env.REACT_APP_BACKEND_URL}`
-            : `https://${process.env.REACT_APP_BACKEND_URL_PROD}`;
+  const backendUrl =
+    process.env.NODE_ENV === "development"
+      ? `http://${process.env.REACT_APP_BACKEND_URL}`
+      : `https://${process.env.REACT_APP_BACKEND_URL_PROD}`;
 
-    
-    const fetchImages = useCallback(async () => {
-        try {
-            const response = await fetch(`${backendUrl}/images`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    uid: uid,
-                    dbName: process.env.REACT_APP_DB_NAME,
-                },
-            });
+  const fetchImages = useCallback(async () => {
+    try {
+      const response = await fetch(`${backendUrl}/images`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          uid: uid,
+          dbName: process.env.REACT_APP_DB_NAME,
+        },
+      });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            const imageArray = await response.json();
-            setImageList(imageArray);
+      const imageArray = await response.json();
+      setImageList(imageArray);
 
-            localStorage.setItem('imageList', JSON.stringify(imageArray));
-        } catch (error) {
-            console.error(error);
-            showSnackbar(`Network or fetch error: ${error.message}`, 'error');
-        } finally {
-            setIsLoading(false);
-        }
-    }, [backendUrl, uid, showSnackbar]);
+      localStorage.setItem("imageList", JSON.stringify(imageArray));
+    } catch (error) {
+      console.error(error);
+      showSnackbar(`Network or fetch error: ${error.message}`, "error");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [backendUrl, uid, showSnackbar]);
 
-    const saveImage = async (image) => {
-        try {
-            setIsLoading(true);
-            const response = await fetch(`${backendUrl}/images/save`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    uid: uid,
-                    dbName: process.env.REACT_APP_DB_NAME,
-                },
-                body: JSON.stringify({ image: image.url }),
-            });
+  const saveImage = async (image) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${backendUrl}/images/save`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          uid: uid,
+          dbName: process.env.REACT_APP_DB_NAME,
+        },
+        body: JSON.stringify({ image: image.url }),
+      });
 
-            if (!response.ok) {
-                const errorText = await response.text();
-                throw new Error(
-                    `Failed to save image. Server responded with: ${errorText}`
-                );
-            }
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Failed to save image. Server responded with: ${errorText}`
+        );
+      }
 
-            const firebaseUrl = await response.text();
-            const downloadedImage = {
-                url: firebaseUrl,
-            };
-            setImageList([...imageList, downloadedImage]);
-            localStorage.setItem(
-                'imageList',
-                JSON.stringify([...imageList, downloadedImage])
-            );
-        } catch (error) {
-            console.error(error);
-            showSnackbar(`Network or save error: ${error.message}`, 'error');
-        } finally {
-            setIsLoading(false);
-        }
-    };
+      const firebaseUrl = await response.text();
+      const downloadedImage = {
+        url: firebaseUrl,
+      };
+      setImageList((prevImageList) => {
+        const updatedImageList = [...prevImageList, downloadedImage];
+        localStorage.setItem("imageList", JSON.stringify(updatedImageList));
+        return updatedImageList;
+      });
+    } catch (error) {
+      console.error(error);
+      showSnackbar(`Network or save error: ${error.message}`, "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const deleteImage = async (path) => {
-        try {
-            const response = await fetch(`${backendUrl}/images`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    uid: uid,
-                    dbName: process.env.REACT_APP_DB_NAME,
-                },
-                body: JSON.stringify({ path: path }),
-            });
+  const deleteImage = async (path) => {
+    try {
+      const response = await fetch(`${backendUrl}/images`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          uid: uid,
+          dbName: process.env.REACT_APP_DB_NAME,
+        },
+        body: JSON.stringify({ path: path }),
+      });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            setImageList(imageList.filter((image) => image.path !== path));
-            localStorage.setItem(
-                'imageList',
-                JSON.stringify(imageList.filter((image) => image.path !== path))
-            );
-        } catch (error) {
-            console.error(error);
-            showSnackbar(`Network or delete error: ${error.message}`, 'error');
-        } finally {
-            setIsLoading(false);
-        }
-    };
+      setImageList((prevImageList) => {
+        const updatedImageList = prevImageList.filter(
+          (image) => image.path !== path
+        );
+        localStorage.setItem("imageList", JSON.stringify(updatedImageList));
+        return updatedImageList;
+      });
+    } catch (error) {
+      console.error(error);
+      showSnackbar(`Network or delete error: ${error.message}`, "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const generateImage = async (imageRequest) => {
-        setImageRequest(imageRequest);
-        try {
-            const response = await fetch(`${backendUrl}/images`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    uid: uid,
-                    dbName: process.env.REACT_APP_DB_NAME,
-                },
-                body: JSON.stringify(imageRequest),
-            });
+  const generateImage = async (imageRequest) => {
+    setImageRequest(imageRequest);
+    try {
+      const response = await fetch(`${backendUrl}/images`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          uid: uid,
+          dbName: process.env.REACT_APP_DB_NAME,
+        },
+        body: JSON.stringify(imageRequest),
+      });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            const imageUrl = await response.text();
+      const imageUrl = await response.text();
 
-            if (imageUrl) {
-                setImageUrl(imageUrl);
-            }
-        } catch (error) {
-            console.log(error);
-            showSnackbar(
-                `Network or generate error: ${error.message}`,
-                'error'
-            );
-        } finally {
-            setIsLoading(false);
-        }
-    };
+      if (imageUrl) {
+        setImageUrl(imageUrl);
+      }
+    } catch (error) {
+      console.log(error);
+      showSnackbar(`Network or generate error: ${error.message}`, "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        const cachedImageUrls = localStorage.getItem('imageList');
-        if (cachedImageUrls) {
-            setImageList(JSON.parse(cachedImageUrls));
-            return;
-        }
-        if (!uid) {
-            return;
-        }
+  useEffect(() => {
+    if (!uid) {
+      return;
+    }
+    const cachedImageUrls = localStorage.getItem("imageList");
+    if (cachedImageUrls) {
+      setImageList(JSON.parse(cachedImageUrls));
+      return;
+    }
+    fetchImages();
+  }, [fetchImages, uid]);
 
-        fetchImages();
-    }, [fetchImages, uid]);
-
-    return {
-        imageList,
-        userPrompt,
-        setUserPrompt,
-        setIsLoading,
-        isLoading,
-        fetchImages,
-        saveImage,
-        deleteImage,
-        generateImage,
-        imageRequest,
-        setImageRequest,
-        imageUrl,
-        setImageUrl,
-    };
+  return {
+    imageList,
+    userPrompt,
+    setUserPrompt,
+    setIsLoading,
+    isLoading,
+    fetchImages,
+    saveImage,
+    deleteImage,
+    generateImage,
+    imageRequest,
+    setImageRequest,
+    imageUrl,
+    setImageUrl,
+  };
 };
