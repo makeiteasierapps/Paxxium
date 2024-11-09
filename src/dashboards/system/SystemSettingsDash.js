26e81362f7d1efde5d20078f440d0150d980631b
Shaun Offenbacher <130625311+makeiteasierapps@users.noreply.github.com>
2024-11-08 23:29:38 -0700

    ui adjustment

--- src/dashboards/system/SystemSettingsDash.js
+++ src/dashboards/system/SystemSettingsDash.js
@@ -1,61 +1,63 @@
-import { useContext, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
-import ConfigFileMenu from './ConfigFileMenu';
-import ConfigFileEditor from './ConfigFileEditor';
-import { AuthContext } from '../../contexts/AuthContext';
-import { SystemContext } from '../../contexts/SystemContext';
-import { MainContainer } from '../../dashboards/styledComponents/DashStyledComponents';
-import ContextResearch from './ContextResearch';
-import SystemHealthCheck from './SystemHealthCheck';
+import { useContext, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import ConfigFileMenu from "./ConfigFileMenu";
+import ConfigFileEditor from "./ConfigFileEditor";
+import { AuthContext } from "../../contexts/AuthContext";
+import { SystemContext } from "../../contexts/SystemContext";
+import { MainContainer } from "../../dashboards/styledComponents/DashStyledComponents";
+import ContextResearch from "./ContextResearch";
+import SystemHealthCheck from "./SystemHealthCheck";
 const SystemSettingsDash = () => {
-    const {
-        fetchConfigFiles,
-        systemAgentMessages,
-        messageSystemAgent,
-        checkSystemHealth,
-    } = useContext(SystemContext);
-    const { uid } = useContext(AuthContext);
+  const {
+    fetchConfigFiles,
+    systemAgentMessages,
+    messageSystemAgent,
+    checkSystemHealth,
+  } = useContext(SystemContext);
+  const { uid } = useContext(AuthContext);
 
-    useEffect(() => {
-        fetchConfigFiles(uid);
-        checkSystemHealth();
-    }, [uid, fetchConfigFiles, checkSystemHealth]);
+  useEffect(() => {
+    fetchConfigFiles(uid);
+    checkSystemHealth();
+    const healthCheckInterval = setInterval(() => {
+      checkSystemHealth();
+    }, 30000);
+    return () => clearInterval(healthCheckInterval);
+  }, [uid, fetchConfigFiles, checkSystemHealth]);
 
-    if (!uid) {
-        return null;
-    }
+  if (!uid) {
+    return null;
+  }
 
-    return (
-        <MainContainer>
-            <Box
-                sx={{
-                    mb: 4,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    width: '100%',
-                }}
-            >
-                <Typography variant="h4">SystemDash</Typography>
-            </Box>
+  return (
+    <MainContainer>
+      <Box
+        sx={{
+          mb: 4,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Typography variant="h4">SystemDash</Typography>
+        <SystemHealthCheck />
+      </Box>
 
-            
+      <Box
+        sx={{
+          width: "100%",
+        }}
+      >
+        <ConfigFileMenu />
+      </Box>
+      <>
+        <ConfigFileEditor uid={uid} />
+      </>
 
-            <Box
-                sx={{
-                    width: '100%',
-                }}
-            >
-                <ConfigFileMenu />
-                <SystemHealthCheck />
-            </Box>
-            <>
-                <ConfigFileEditor uid={uid} />
-            </>
-
-            <Box width="100%">
-                <ContextResearch />
-                {/* <Chat
+      <Box width="100%">
+        <ContextResearch />
+        {/* <Chat
                     sx={{
                         width: '100%',
                         height: '90vh',
@@ -63,9 +65,9 @@ const SystemSettingsDash = () => {
                     messages={systemAgentMessages}
                     onSendMessage={messageSystemAgent}
                 /> */}
-            </Box>
-        </MainContainer>
-    );
+      </Box>
+    </MainContainer>
+  );
 };
 
 export default SystemSettingsDash;
