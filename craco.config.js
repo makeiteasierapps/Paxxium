c63c46b2428e6003330631db980ec2c4be012bf9
Shaun Offenbacher <makeiteasierapps@gmail.com>
2025-01-31 16:15:33 -0700

    added craco.manifest

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
+
+const config = {
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            if (env === 'production') {
+                webpackConfig.plugins.push(
+                    new WorkboxWebpackPlugin.InjectManifest({
+                        swSrc: './src/service-worker.js',
+                        swDest: 'service-worker.js',
+                        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+                    })
+                );
+            }
+            return webpackConfig;
+        },
+    },
+};
+
+export default config;
